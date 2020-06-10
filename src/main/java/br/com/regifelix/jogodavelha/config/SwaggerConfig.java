26193f6290e66255764328d5f4975939e34dfa6c
Reginaldo Donizete Felix <reginaldo.felix@unicid.edu.br>
2020-06-10 10:46:15 -0300

    Criado configuração do swagger para documentação da API

--- src/main/java/br/com/regifelix/jogodavelha/config/SwaggerConfig.java
+++ src/main/java/br/com/regifelix/jogodavelha/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.Generated;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,7 +20,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-
+@Generated
 public class SwaggerConfig {	
 	
 	
